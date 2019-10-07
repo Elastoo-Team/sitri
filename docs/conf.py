4c38323932f101cf30ee24059fa47f2fdbbbe698
Alexander Lavrov <egnod@ya.ru>
2019-10-07 13:12:59 +0300

    moves

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 import typing
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../"))
 sys.setrecursionlimit(1500)
 
 # -- Project information -----------------------------------------------------
