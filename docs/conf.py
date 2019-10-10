7cbc8ec38516fd380092ae3dbff7b400b638aa54
Alexander Lavrov <egnod@ya.ru>
2019-10-11 01:23:41 +0300

    Bump version: 0.5.3 → 0.5.4

--- docs/conf.py
+++ docs/conf.py
@@ -24,8 +24,8 @@ copyright = "2019, Aleksander Lavrov"
 author = "Aleksander Lavrov"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.3"
-version = "0.5.3"
+release = "0.5.4"
+version = "0.5.4"
 
 # -- General configuration ---------------------------------------------------
 
