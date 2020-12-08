62f0ab9682fd4e92d12dd0f00baac7bf43b004f6
Alexander Lavrov <alexander.lavrov@elastoo.com>
2020-12-09 00:15:13 +0300

    fix version on conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,8 +24,8 @@ copyright = "2019-2020, Aleksander Lavrov"
 author = "Aleksander Lavrov"
 
 # The full version, including alpha/beta/rc tags
-release = "0.10.0"
-version = "0.10.0"
+release = "0.10.2"
+version = "0.10.2"
 
 # -- General configuration ---------------------------------------------------
 
