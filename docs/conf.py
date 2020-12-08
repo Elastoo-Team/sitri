c85e3868005218e8713192716ab88a0b4759dd44
Alexander Lavrov <alexander.lavrov@elastoo.com>
2020-12-09 00:50:02 +0300

    Bump version: 0.10.4 → 0.10.5

--- docs/conf.py
+++ docs/conf.py
@@ -24,8 +24,8 @@ copyright = "2019-2020, Aleksander Lavrov"
 author = "Aleksander Lavrov"
 
 # The full version, including alpha/beta/rc tags
-release = "0.10.4"
-version = "0.10.4"
+release = "0.10.5"
+version = "0.10.5"
 
 # -- General configuration ---------------------------------------------------
 
