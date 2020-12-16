38f6ef87159b09a6b176890224ecdfd40eeba03d
Alexander Lavrov <alexander.lavrov@elastoo.com>
2020-12-16 20:04:28 +0300

    add vault_kv secretion to getting_started.rst

--- sitri/providers/contrib/vault/__init__.py
+++ sitri/providers/contrib/vault/__init__.py
@@ -1,1 +1,7 @@
+"""
+.. module:: providers.contrib.vault.__init__
+   :synopsis: Contrib additional functional with vault
+.. moduleauthor:: Aleksander Lavrov <github.com/egnod>
+"""
+
 from sitri.providers.contrib.vault.vault_kv import VaultKVConfigProvider
