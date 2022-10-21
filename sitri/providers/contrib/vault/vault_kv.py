3af4f0628c1a804940fbe0a419e8f4dce70ed881
Alexander Lavrov <admin@egnod.dev>
2022-10-21 14:02:54 +0300

    add vault kv 2 storage

--- sitri/providers/contrib/vault/vault_kv.py
+++ sitri/providers/contrib/vault/vault_kv.py
@@ -3,15 +3,13 @@ from __future__ import annotations
 import typing as t
 
 import hvac
+from hvac.api.vault_api_base import VaultApiBase
 
 from sitri.providers.base import ConfigProvider
 
 
 class VaultKVConfigProvider(ConfigProvider):
-    """Config provider for vault_kv storage.
-
-    Only for kv first version at the moment.
-    """
+    """Config provider for vault_kv storage."""
 
     provider_code = "vault_kv"
 
@@ -20,20 +18,23 @@ class VaultKVConfigProvider(ConfigProvider):
         vault_connector: t.Callable[[], hvac.Client],
         mount_point: str | None = None,
         secret_path: str | None = None,
+        version: int | None = 1,
         *args: t.Any,
         **kwargs: t.Any,
     ) -> None:
         """
         :param vault_connector: function return connection to Vault
-        :param mount_point: default vault_kv kv1 storage mount point
+        :param mount_point: default vault_kv storage mount point
         :param secret_path: default path to the secret in mounted
         """
         super().__init__(*args, **kwargs)
+        assert version in (1, 2)
 
         self._vault_get = vault_connector
         self._vault_hvac_instance = None
         self._mount_point = mount_point
         self._secret_path = secret_path
+        self._version = version
 
     @property
     def _vault(self) -> hvac.Client:
@@ -45,6 +46,10 @@ class VaultKVConfigProvider(ConfigProvider):
             self._vault_hvac_instance = self._vault_get()
 
         return self._vault_hvac_instance
+
+    @property
+    def _vault_kv(self) -> VaultApiBase:
+        return self._vault.secrets.kv.v1 if self._version == 1 else self._vault.secrets.kv.v2
 
     def get(
         self, key: str, mount_point: str | None = None, secret_path: str | None = None, **kwargs: t.Any
@@ -65,7 +70,7 @@ class VaultKVConfigProvider(ConfigProvider):
             "mount_point": mount_point if mount_point else self._mount_point,
         }
 
-        response = self._vault.secrets.kv.v1.read_secret(**request_params)
+        response = self._vault_kv.read_secret(**request_params)
 
         return response["data"].get(key)
 
@@ -84,6 +89,6 @@ class VaultKVConfigProvider(ConfigProvider):
             "mount_point": mount_point if mount_point else self._mount_point,
         }
 
-        response = self._vault.secrets.kv.v1.read_secret(**request_params)
+        response = self._vault_kv.read_secret(**request_params)
 
         return list(response["data"].keys())
